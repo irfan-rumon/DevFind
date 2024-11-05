2c1b72b1b566c8dd0aa88d056b00bfc965d74422
irfan-rumon <irfannisho8571@gmail.com>
2024-11-05 12:31:04 +0600

    Modify: developer model

--- custom_addons/devfind/models/developer.py
+++ custom_addons/devfind/models/developer.py
@@ -1,8 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Developer(models.Model):
     _name = 'devfind.developer'
     _description = 'Developer Information'
+    _sql_constraints = [
+        ('email_unique', 'unique(email)', 'Email must be unique!')
+    ]
 
     name = fields.Char(string='Name', required=True)
     email = fields.Char(string='Email', required=True)
@@ -18,3 +22,20 @@ class Developer(models.Model):
 
     min_hourly_rate = fields.Float(string='Min Hourly Rate', required=True)
     max_hourly_rate = fields.Float(string='Max Hourly Rate', required=True)
+
+    # Computed field to show technology count
+    technology_count = fields.Integer(
+        string='Technology Count',
+        compute='_compute_technology_count'
+    )
+
+    @api.depends('technology_ids')
+    def _compute_technology_count(self):
+        for record in self:
+            record.technology_count = len(record.technology_ids)
+
+    @api.constrains('min_hourly_rate', 'max_hourly_rate')
+    def _check_hourly_rates(self):
+        for record in self:
+            if record.min_hourly_rate > record.max_hourly_rate:
+                raise ValidationError("Minimum hourly rate cannot be greater than maximum rate!")
