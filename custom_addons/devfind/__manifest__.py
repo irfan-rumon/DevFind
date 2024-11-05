90506e12db162dda9f56605d522208acef897cfe
irfan-rumon <irfannisho8571@gmail.com>
2024-11-05 17:11:26 +0600

    fix: menu layout

--- custom_addons/devfind/__manifest__.py
+++ custom_addons/devfind/__manifest__.py
@@ -5,12 +5,10 @@
     'description': 'Module for finding and managing developers', 
     'category': 'Human Resources',  
     'author': 'A S M IRFAN', 
-    'depends': ['base'],
+    'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
-        'views/developer_views.xml',
-        'views/technology_views.xml',
-        'views/tech_dev_mapper_views.xml',
+        'views/developer_views.xml'
     ],
     'installable': True,
     'application': True,
