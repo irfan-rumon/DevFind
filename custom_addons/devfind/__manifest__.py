e4008bfc6d3344d311841962df190e956bc43eea
irfan-rumon <irfannisho8571@gmail.com>
2024-11-05 11:56:42 +0600

    fix codebase directory structure

---
+++ custom_addons/devfind/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'DevFind',
+    'version': '1.0',
+    'summary': 'Developer Finding App',  
+    'description': 'Module for finding and managing developers', 
+    'category': 'Human Resources',  
+    'author': 'A S M IRFAN', 
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
