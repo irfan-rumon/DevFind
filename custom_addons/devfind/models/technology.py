2887d2051e3ad940e726f3e7e14752617e63bea6
irfan-rumon <irfannisho8571@gmail.com>
2024-11-05 12:21:34 +0600

    Modify: Technology model

--- custom_addons/devfind/models/technology.py
+++ custom_addons/devfind/models/technology.py
@@ -1,7 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Technology(models.Model):
     _name = 'devfind.technology'
     _description = 'Technology Information'
+    _sql_constraints = [
+        ('name_unique', 'unique(name)', 'Technology name must be unique!')
+    ]
 
     name = fields.Char(string='Technology Name', required=True)
+    developer_ids = fields.Many2many(
+        'devfind.developer',
+        'tech_dev_mapper_rel',  # Same table name as in Developer model
+        'technology_id',
+        'developer_id',
+        string='Developers'
+    )
