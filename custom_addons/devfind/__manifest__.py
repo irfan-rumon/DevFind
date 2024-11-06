47585273140ecd4481bf0c6c85f7470d38d9bdd9
irfan-rumon <irfannisho8571@gmail.com>
2024-11-06 12:12:21 +0600

    Define security policies for developers

--- custom_addons/devfind/__manifest__.py
+++ custom_addons/devfind/__manifest__.py
@@ -1,17 +1,24 @@
 {
     'name': 'devfind',
     'version': '1.0',
-    'summary': 'Developer Finding App',  
+    'summary': 'Developer Finding Platform',  
     'description': 'Module for finding and managing developers', 
-    'category': 'Human Resources',  
+    'category': 'Services',  
     'author': 'A S M IRFAN', 
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', 'mail'],
     'data': [
+         # Load security first
+        'security/security_groups.xml',
         'security/ir.model.access.csv',
+        # Then load views
+        'views/auth_templates.xml',
+        'views/menus.xml',
         'views/developer_views.xml',
         'views/technology_views.xml',
-        'views/menus.xml'
+        
+        
     ],
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
