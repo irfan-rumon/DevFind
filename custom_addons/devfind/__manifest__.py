7bf367718a3eaf9a260e2cacb1732337d761ae74
irfan-rumon <irfannisho8571@gmail.com>
2024-11-08 19:49:14 +0600

    Fix: css error isuue by settinp up another fresh database

--- custom_addons/devfind/__manifest__.py
+++ custom_addons/devfind/__manifest__.py
@@ -5,19 +5,25 @@
     'description': 'Module for finding and managing developers', 
     'category': 'Services',  
     'author': 'A S M IRFAN', 
-    'depends': ['base', 'web'],
+    'depends': ['base'],
     'data': [
          # Load security first
         'security/security_groups.xml',
         'security/ir.model.access.csv',
         # Then load views
         'views/auth_templates.xml',
-        'views/menus.xml',
         'views/developer_views.xml',
+        'views/developer_templates.xml',
         'views/technology_views.xml',
-        
-        
+        'views/menus.xml'
     ],
+    'assets': {
+        'web.assets_backend': [
+            'devfind/static/src/components/developer_list/developer_list.js',
+            'devfind/static/src/components/developer_list/developer_list.xml',
+            'devfind/static/src/components/developer_list/developer_list.scss',
+        ],
+    },
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
